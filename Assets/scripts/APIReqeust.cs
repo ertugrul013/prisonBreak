d0e3d54c46f41466f2b99c2b9d1112f7f544559c
ertugrul013 <ertugrul013@hotmail.com>
2019-03-12 17:24:56 +0100

    added Simple JSON libary and started work on the API reqeust

---
+++ Assets/scripts/APIReqeust.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using SimpleJSON;
+
+public class APIReqeust : MonoBehaviour
+{
+    [Range(5, 100)] public int AmountOfPictures;
+    public string URL = "http://shibe.online/api/shibes?count=&urls=true&httpsUrls=true";
+    public string[] ImageURL;
+    public List<Texture2D> Image;
+
+    WebClient _webClient = new WebClient();
+
+    private void Start()
+    {
+        ImageURL = new string[AmountOfPictures];
+        
+        URL = URL.Insert(37, AmountOfPictures.ToString());
+        StartCoroutine(GetImages());
+    }
+
+    IEnumerator GetImages()
+    {
+        for (int i = 0; i < AmountOfPictures; i++)
+        {
+            Texture2D tex;
+            tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+            using (WWW www = new WWW(URL))
+            {
+                yield return www;
+                www.LoadImageIntoTexture(tex);
+                Image.Add(tex);
+            }
+        }
+
+    }
+
+}
